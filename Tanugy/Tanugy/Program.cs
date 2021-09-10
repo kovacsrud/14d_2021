0645fba98cc57ae590e3906c3521f9d6396fc963
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2021-09-10 09:18:40 +0200

    Tanügy mintaprogram

---
+++ Tanugy/Tanugy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanugy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tanulo> tanulok = new List<Tanulo>();
+            List<Tanar> tanarok = new List<Tanar>();
+
+            tanulok.Add(new Tanulo("Kiss Ubul", 2003));
+            tanulok.Add(new Tanulo("Vozár Ella", 2004));
+
+            tanarok.Add(new Tanar("Kovács Győző", "matematika"));
+            tanarok.Add(new Tanar("Nagy Elek", "irodalom"));
+
+            SuliOsztaly osztaly9a = new SuliOsztaly(tanulok,tanarok,"9a");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tanugy/Tanugy/SuliOsztaly.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanugy
+{
+    public class SuliOsztaly
+    {
+        public List<Tanulo> Tanulok { get; set; }
+        public List<Tanar> Tanarok { get; set; }
+        public string Osztalynev { get; set; }
+
+        public SuliOsztaly(List<Tanulo> tanulok, List<Tanar> tanarok, string osztalynev)
+        {
+            Tanulok = tanulok;
+            Tanarok = tanarok;
+            Osztalynev = osztalynev;
+        }
+
+        public void TanuloLista()
+        {
+            foreach (var i in Tanulok)
+            {
+                Console.WriteLine($"Név:{i.Nev},{i.SzuletesiEv}");
+            }
+        }
+
+       
+
+
+    }
+}
